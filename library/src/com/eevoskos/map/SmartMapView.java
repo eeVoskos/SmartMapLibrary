ab3f3d02e6ddf805de7893f6a1dd258bd9627826
eeVoskos <eeVoskos@gmail.com>
2012-09-19 02:16:55 +0300

    Made SmartMapView clickable by default

--- library/src/com/eevoskos/map/SmartMapView.java
+++ library/src/com/eevoskos/map/SmartMapView.java
@@ -16,121 +16,119 @@ import com.google.android.maps.MapView;
  * 
  * @author Stratos Theodorou
  */
-public class SmartMapView extends MapView implements OnGestureListener,
-		OnDoubleTapListener {
+public class SmartMapView extends MapView implements OnGestureListener, OnDoubleTapListener {
 
-	/**
-	 * Interface for listening for long-press events of the {@link SmartMapView},
-	 * translated into {@link GeoPoint}s.
-	 * 
-	 * @author Stratos Theodorou
-	 * 
-	 */
-	public interface OnLongPressListener {
+    /**
+     * Interface for listening for long-press events of the {@link SmartMapView}
+     * , translated into {@link GeoPoint}s.
+     * 
+     * @author Stratos Theodorou
+     * 
+     */
+    public interface OnLongPressListener {
 
-		/**
-		 * Called when a long-press event has been captured on the
-		 * {@link SmartMapView}.
-		 * 
-		 * @param point
-		 *          The location of the long-press event, translated into a
-		 *          {@link GeoPoint}.
-		 */
-		public void onLongPress(GeoPoint point);
-	}
+        /**
+         * Called when a long-press event has been captured on the
+         * {@link SmartMapView}.
+         * 
+         * @param point
+         *            The location of the long-press event, translated into a
+         *            {@link GeoPoint}.
+         */
+        public void onLongPress(GeoPoint point);
+    }
 
-	private GestureDetector mGestureDetector;
-	private OnLongPressListener mOnLongPressListener;
+    private GestureDetector mGestureDetector;
+    private OnLongPressListener mOnLongPressListener;
 
-	public SmartMapView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init();
-	}
+    public SmartMapView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
 
-	public SmartMapView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		init();
-	}
+    public SmartMapView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
 
-	public SmartMapView(Context context, String apiKey) {
-		super(context, apiKey);
-		init();
-	}
+    public SmartMapView(Context context, String apiKey) {
+        super(context, apiKey);
+        init();
+    }
 
-	private void init() {
-		mGestureDetector = new GestureDetector(getContext(), this);
-		mGestureDetector.setOnDoubleTapListener(this);
-	}
+    private void init() {
+        setClickable(true);
+        mGestureDetector = new GestureDetector(getContext(), this);
+        mGestureDetector.setOnDoubleTapListener(this);
+    }
 
-	/**
-	 * Set a long-press event listener to the {@link SmartMapView}.
-	 * 
-	 * @param onLongPressListener
-	 *          The long-press listener to assign.
-	 */
-	public void setOnLongPressListener(OnLongPressListener onLongPressListener) {
-		mOnLongPressListener = onLongPressListener;
-	}
+    /**
+     * Set a long-press event listener to the {@link SmartMapView}.
+     * 
+     * @param onLongPressListener
+     *            The long-press listener to assign.
+     */
+    public void setOnLongPressListener(OnLongPressListener onLongPressListener) {
+        mOnLongPressListener = onLongPressListener;
+    }
 
-	public boolean onTouchEvent(MotionEvent ev) {
-		if (mGestureDetector.onTouchEvent(ev)) {
-			return true;
-		}
-		return super.onTouchEvent(ev);
-	}
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (mGestureDetector.onTouchEvent(ev)) {
+            return true;
+        }
+        return super.onTouchEvent(ev);
+    }
 
-	/* OnDoubleTapListener */
+    /* OnDoubleTapListener */
 
-	@Override
-	public boolean onDoubleTap(MotionEvent e) {
-		getController().zoomInFixing((int) e.getX(), (int) e.getY());
-		return true;
-	}
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        getController().zoomInFixing((int) e.getX(), (int) e.getY());
+        return true;
+    }
 
-	@Override
-	public boolean onDoubleTapEvent(MotionEvent e) {
-		return false;
-	}
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
 
-	@Override
-	public boolean onSingleTapConfirmed(MotionEvent e) {
-		return false;
-	}
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
 
-	/* OnGestureListener */
+    /* OnGestureListener */
 
-	@Override
-	public boolean onDown(MotionEvent e) {
-		return false;
-	}
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
 
-	@Override
-	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-			float velocityY) {
-		return false;
-	}
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
 
-	@Override
-	public void onLongPress(MotionEvent e) {
-		if (mOnLongPressListener != null) {
-			mOnLongPressListener.onLongPress(getProjection().fromPixels(
-					(int) e.getX(), (int) e.getY()));
-		}
-	}
+    @Override
+    public void onLongPress(MotionEvent e) {
+        if (mOnLongPressListener != null) {
+            mOnLongPressListener.onLongPress(getProjection().fromPixels((int) e.getX(),
+                    (int) e.getY()));
+        }
+    }
 
-	@Override
-	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-			float distanceY) {
-		return false;
-	}
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
 
-	@Override
-	public void onShowPress(MotionEvent e) {
-	}
+    @Override
+    public void onShowPress(MotionEvent e) {
+    }
 
-	@Override
-	public boolean onSingleTapUp(MotionEvent e) {
-		return false;
-	}
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
 
 }
